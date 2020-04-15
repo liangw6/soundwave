85a0b8996a396052fb3be42882ec40caa880dec2
Arthur <liangw6@cs.washington.edu>
2020-04-15 15:28:36 -0700

    Initial Commit

---
+++ FoodTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodTracker
+//
+//  Created by Liang Arthur on 4/15/20.
+//  Copyright © 2020 Liang Arthur. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
